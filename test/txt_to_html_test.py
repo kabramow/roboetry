dba4575d1108aabeedbf607adac567a12caafed9
kabramow <kabramow@indiana.edu>
2017-11-13 12:27:34 -0500

    tidied up tests

--- test/txt_to_html_test.py
+++ test/txt_to_html_test.py
@@ -1,12 +1,8 @@
 import txt_to_html, nltk
 
 
-txt_to_html.wrap_string_in_html("test2", "american_fairy_tales_baum.txt")
+txt_to_html.wrap_string_in_html("test4", "american_fairy_tales_baum.txt")
 
-# nltk.download('punkt')
-# sentence = """At eight \"o'clock on Thursday morning\"
-#  Mr. Arthur couldn't feel very good. And this is another sentence."""
-# tokens = nltk.word_tokenize(sentence)
-# print(tokens)
 
-txt_to_html.poem_generator(["1","2","3","4"])
+
+
